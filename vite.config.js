774d8b23de90d29c0b968455f58c691b9843f2aa
daritrujillo <darineltrujillo25@gmail.com>
2025-10-22 23:10:13 -0600

    Corrige base en vite.config para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// IMPORTANT: Replace NOMBRE_DEL_REPO with your GitHub repo name before deploying to Pages.
+// 👇 El nombre debe coincidir exactamente con tu repositorio en GitHub
 export default defineConfig({
   plugins: [vue()],
-  base: '/NOMBRE_DEL_REPO/'
+  base: '/SPA_Vue3_Pinia_Carrito_CRUD_equipo/',
 })
+
